fea6f1f9b707a5bcfab6752b8dd34c056e0ebcd3
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-04-20 18:10:28 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K7/SuitcasePacking/MrSquare.java
@@ -0,0 +1,12 @@
+package org.example.K7.SuitcasePacking;
+
+//Mr. Square is going on a holiday. He wants to bring 2 of his favorite squares with him, so he put them in his rectangle suitcase.
+//Write a function that, given the size of the squares and the suitcase, return whether the squares can fit inside the suitcase.
+
+public class MrSquare {
+    public static Boolean fit_in(int a, int b, int m, int n) {
+
+
+        return true;
+    }
+}
