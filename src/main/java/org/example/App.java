15bfb5953ed7aba719c6b2599263ae259c221099
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-04-22 17:08:13 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,26 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App { public static void main(String[] args ) {
 
-    int start = 125;
-    int end = 132;
+    String s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
 
-    int size = end - start + 1;
-    int nr = start;
-    ArrayList<String> help = new ArrayList<>();
-    ArrayList<String> help1 = new ArrayList<>();
-    for (int i = 0; i < size; i++) {
-        help.add(String.valueOf(start));
-        start++;
-    };
+    Map<String, String> mymap = new HashMap<>();
+
+    String[] pairs = s.split(";");
+    for (int i = 0; i < pairs.length; i++) {
+        String pair = pairs[i];
+        String[] keyValue = pair.split(":");
+        mymap.put(keyValue[0], keyValue[1]);
+    }
+
+
 
 
     }
 }
+
