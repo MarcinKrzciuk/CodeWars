1796326f6c1fa2f596e0e28c83e50e7b846b9726
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-04-27 18:53:26 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/K7/FindMeetingRoom/TheOffice.java
+++ src/main/java/org/example/K7/FindMeetingRoom/TheOffice.java
@@ -2,18 +2,8 @@ package org.example.K7.FindMeetingRoom;
 
 public class TheOffice {
     public static Object meeting(char[] x) {
-        for (int i = 0; i < x.length; i++) {
-            if (x[i] == 'O') {
-                return i;
-            }
-        }
+        int i = new String(x).indexOf('O');
+        return i < 0 ? "None available!" : i;
 
-        for (int i = 0; i < x.length; i++) {
-            if ((x[0] == x[i]) && (x[i] == 'X')) {
-                return "None available!";
-            }
-        }
-
-        return 0;
     }
 }
