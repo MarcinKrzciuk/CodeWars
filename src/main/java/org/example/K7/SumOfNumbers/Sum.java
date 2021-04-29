a23d5b4a7490d5df7cfdbd5e3ff87a4fb5783df0
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-04-29 13:16:46 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K7/SumOfNumbers/Sum.java
@@ -0,0 +1,12 @@
+package org.example.K7.SumOfNumbers;
+
+public class Sum {
+    public int GetSum(int a, int b) {
+        int res = 0;
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            res += i;
+        }
+        return a == b ? a : res;
+
+    }
+}
