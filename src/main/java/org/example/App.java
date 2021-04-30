16de4bccdd0e6569ad0e731703c4ac954135ed79
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-04-30 19:36:48 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,69 +1,47 @@
 package org.example;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
-    private static final Map<String, Character> morseAlphabet = new HashMap<>();
-
-    static {
-        morseAlphabet.put(".-", 'A');
-        morseAlphabet.put("-...", 'B');
-        morseAlphabet.put("-.-.", 'C');
-        morseAlphabet.put("-..", 'D');
-        morseAlphabet.put(".", 'E');
-        morseAlphabet.put("..-.", 'F');
-        morseAlphabet.put("--.", 'G');
-        morseAlphabet.put("....", 'H');
-        morseAlphabet.put("..", 'I');
-        morseAlphabet.put(".---", 'J');
-        morseAlphabet.put("-.-", 'K');
-        morseAlphabet.put(".-..", 'L');
-        morseAlphabet.put("--", 'M');
-        morseAlphabet.put("-.", 'N');
-        morseAlphabet.put("---", 'O');
-        morseAlphabet.put(".--.", 'P');
-        morseAlphabet.put("--.-", 'Q');
-        morseAlphabet.put(".-.", 'R');
-        morseAlphabet.put("...", 'S');
-        morseAlphabet.put("-", 'T');
-        morseAlphabet.put("..-", 'U');
-        morseAlphabet.put("...-", 'V');
-        morseAlphabet.put(".--", 'W');
-        morseAlphabet.put("-..-", 'X');
-        morseAlphabet.put("-.--", 'Y');
-        morseAlphabet.put("--..", 'Z');
-        morseAlphabet.put("-----", '0');
-        morseAlphabet.put(".----", '1');
-        morseAlphabet.put("..---", '2');
-        morseAlphabet.put("...--", '3');
-        morseAlphabet.put("....-", '4');
-        morseAlphabet.put(".....", '5');
-        morseAlphabet.put("-....", '6');
-        morseAlphabet.put("--...", '7');
-        morseAlphabet.put("---..", '8');
-        morseAlphabet.put("----.", '9');
-    }
-
     public static void main(String[] args) {
+        int n = 25;
+        int d = 1;
+        String check = String.valueOf(d);
 
-        String txt = "···· · −·−−   ·−−− ··− −·· ·";
-        String txt2 = decode(txt);
-        System.out.println(decode(txt2));
+        int count = 0;
 
-    }
+        ArrayList<Integer> nr = new ArrayList<>();
+        ArrayList<String> nr1 = new ArrayList<>();
 
-    private static String decode(String morse) {
-        StringBuilder decoded = new StringBuilder();
-
-        String[] words = morse.split(" ");
-        for (String word : words) {
-            decoded.append(' ');
-            String[] letters = word.split(" ");
-            for (String letter : letters) {
-                decoded.append(morseAlphabet.get(letter));
-            }
+        for (int i = 0; i <= n; i++){
+            nr.add(i);
         }
 
-        return decoded.substring(1);
+        System.out.println(nr);
+
+        for (int i = 0; i < nr.size(); i++){
+            nr.set(i, nr.get(i) * nr.get(i));
+            nr1.add(nr.get(i).toString());
+        }
+
+        System.out.println(nr1);
+
+        for (int i = 0; i < nr1.size(); i++) {
+            if (nr1.get(i).contains(check)) {
+               for (int j = 0; j < nr1.get(i).length(); j++) {
+
+               }
+            }
+
+        }
+
+        System.out.println(count);
+
     }
 }
+
+
+
+
+
