b0b8a83be5ad424fcae76c930244d05a871d46a6
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-01 08:02:21 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/K7/CounttheDigit/CountDig.java
+++ src/main/java/org/example/K7/CounttheDigit/CountDig.java
@@ -1,8 +1,19 @@
 package org.example.K7.CounttheDigit;
 
+
 public class CountDig {
     public static int nbDig(int n, int d) {
-        // your code
-        return 1;
+
+            int c = 0;
+            for (int i = 0; i < n + 1; i++) {
+                int temp = i * i;
+                while (true) {
+                    if (temp % 10 == d)
+                        c++;
+                    if ((temp /= 10) == 0)
+                        break;
+                }
+            }
+        return c;
     }
 }
