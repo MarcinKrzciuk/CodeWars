c64d945505fb5ead84689deb81f55746b3513e7f
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-04 06:49:01 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K6/WhoLikesIt/Solution/Solution.java
@@ -0,0 +1,28 @@
+package org.example.K6.WhoLikesIt.Solution;
+
+public class Solution {
+    public static String whoLikesIt(String... names) {
+
+        if (names.length == 0) {
+            return "no one likes this";
+        }
+
+        if (names.length == 1) {
+            return names[0] + " likes this";
+        }
+
+        if (names.length == 2) {
+            return names[0] + " and " + names[1] + " like this";
+        }
+
+        if (names.length == 3) {
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        }
+
+        if (names.length > 3) {
+            int counter = names.length - 2;
+            return names[0] + ", " + names[1] + " and " + counter + " others like this";
+        }
+        return "";
+    }
+}
