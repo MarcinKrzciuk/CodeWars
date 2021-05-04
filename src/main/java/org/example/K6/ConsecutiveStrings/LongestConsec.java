f5c0591a993c28567b7abdbd7d3e33de9f3efe7b
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-04 07:08:03 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K6/ConsecutiveStrings/LongestConsec.java
@@ -0,0 +1,25 @@
+package org.example.K6.ConsecutiveStrings;
+
+//        strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+//
+//        Concatenate the consecutive strings of strarr by 2, we get:
+//
+//        treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+//        folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+//        trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+//        blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+//        abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+//
+//        Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+//        The first that came is "folingtrashy" so
+//        longest_consec(strarr, 2) should return "folingtrashy".
+//
+//        In the same way:
+//        longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+public class LongestConsec {
+    public static String longestConsec(String[] strarr, int k) {
+        // your code
+        return "";
+    }
+}
