b3418a3b4bd8e9f304d5f2707536de32c071054b
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-11 14:49:25 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K6/CharacterWithLongestConsecutiveRepetition/Solution.java
@@ -0,0 +1,9 @@
+package org.example.K6.CharacterWithLongestConsecutiveRepetition;
+
+public class Solution {
+    public static Object[] longestRepetition(String s) {
+
+
+        return new Object[]{"", 0};
+    }
+}
