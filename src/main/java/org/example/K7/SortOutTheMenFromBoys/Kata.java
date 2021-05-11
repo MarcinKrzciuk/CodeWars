092b516e0ea3b5b24518ff0b3d4863e60eac5df5
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-11 14:46:02 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/K7/SortOutTheMenFromBoys/Kata.java
+++ src/main/java/org/example/K7/SortOutTheMenFromBoys/Kata.java
@@ -2,6 +2,8 @@ package org.example.K7.SortOutTheMenFromBoys;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Kata {
     public static int[] menFromBoys(final int[] values) {
@@ -12,14 +14,15 @@ public class Kata {
         for (int i = 0; i < values.length; i++) {
             if (values[i] %2 == 0) {
                 list.add(values[i]);
-            } else {
+            } else if (values[i] %2 != 0){
                 list1.add(values[i]);
             }
         }
         Collections.sort(list);
         Collections.sort(list1, Collections.reverseOrder());
         list.addAll(list1);
+        List<Integer> newList = list.stream().distinct().collect(Collectors.toList());
 
-        return list.stream().mapToInt(i -> i).toArray();
+        return newList.stream().mapToInt(i -> i).toArray();
     }
 }
