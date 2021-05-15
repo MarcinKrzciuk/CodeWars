0236704a4e64274ddc7d270da022394639c389cb
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-15 12:02:45 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K5/SortOneThreeTwo/Dinglemouse.java
@@ -0,0 +1,10 @@
+package org.example.K5.SortOneThreeTwo;
+
+public class Dinglemouse {
+    public static int[] sort(final int array[]) {
+        // Your code here
+        return new int[0];
+    }
+
+
+}
