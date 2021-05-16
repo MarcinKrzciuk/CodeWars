8bd7810d83bc22771d35b6dd33b090d560f4d91e
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-16 10:40:13 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

---
+++ src/main/java/org/example/K7/DivisorsOfNumber/FindDivisor.java
@@ -0,0 +1,15 @@
+package org.example.K7.DivisorsOfNumber;
+
+public class FindDivisor {
+    public long numberOfDivisors(int n) {
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n %i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
