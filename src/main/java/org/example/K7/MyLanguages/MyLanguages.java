eca2a153f0e166c2acc1d1ed4509bd0663cee676
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-18 15:33:51 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/K7/MyLanguages/MyLanguages.java
+++ src/main/java/org/example/K7/MyLanguages/MyLanguages.java
@@ -1,17 +1,25 @@
 package org.example.K7.MyLanguages;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyLanguages {
     public static List<String> myLanguages(final Map<String, Integer> results) {
         ArrayList<String> myLanguages = new ArrayList<>();
-        for (Map.Entry<String, Integer> exam : results.entrySet()) {
-            if (exam.getValue() >= 60) {
+        List<Map.Entry<String, Integer> > list = new LinkedList<Map.Entry<String, Integer> >(results.entrySet());
+        System.out.println(list);
+        Collections.sort(list, (i1, i2) -> i1.getValue().compareTo(i2.getValue()));
+        System.out.println(list);
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+
+        for (Map.Entry<String, Integer> exam : temp.entrySet()) {
+            if (exam.getValue() > 59) {
                 myLanguages.add(exam.getKey());
             }
         }
+        Collections.reverse(myLanguages);
         return myLanguages;
     }
 }
