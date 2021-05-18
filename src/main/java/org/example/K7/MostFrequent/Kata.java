e282cf7fe09b9ebdc418cbb419fb230de278fc15
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-18 17:05:20 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/K7/MostFrequent/Kata.java
+++ src/main/java/org/example/K7/MostFrequent/Kata.java
@@ -1,7 +1,31 @@
 package org.example.K7.MostFrequent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Kata {
     public static int mostFrequentItemCount(int[] collection) {
-        return 0;
+        HashMap<Integer, Integer> hp = new HashMap<>();
+        for(int i = 0; i < collection.length; i++) {
+            int key = collection[i];
+            if (hp.containsKey(key)) {
+                int freq = hp.get(key);
+                freq++;
+                hp.put(key, freq);
+            } else {
+                hp.put(key, 1);
+            }
+        }
+        int max_count = 0, res = -1;
+
+        for(Map.Entry<Integer, Integer> val : hp.entrySet()) {
+            if (max_count < val.getValue()) {
+                res = val.getKey();
+                max_count = val.getValue();
+            }
+        }
+
+        return max_count;
+
     }
 }
