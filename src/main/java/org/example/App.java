0598e3410157b1e8ea50f7c6b0ed979f0205be78
Marcin Krzciuk <67062802+MarcinKrzciuk@users.noreply.github.com>
2021-05-24 19:57:06 +0200

    Merge branch 'master' of https://github.com/MarcinKrzciuk/CodeWars

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,47 @@
 package org.example;
 
-import java.util.*;
+
+import java.text.ParseException;
+import java.util.ArrayList;
 
 public class App {
+    public static void main(String[] args) throws ParseException {
+//        int [] array = new int[]{1,11,3,9};
+//        ArrayList<String> list = new ArrayList<>();
+//
+//        String[] onedigit = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+//        String[] twodigits = new String[] {"", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+//        String[] multipleoftens = new String[] {"",  "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+//        String[] poweroftens = new String[] {"Hundred", "Thousand"};
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] < 10) {
+//                list.add(onedigit[array[i]]);
+//            }
+//        }
+//
+//        System.out.println(list);
 
+        int num = 999;
+        int h;
+        ArrayList<Integer> list = new ArrayList<>();
+        String nr = String.valueOf(num);
+//        for (int i = 0; i < nr.length(); i++) {
+//            h = num % 10;
+//            System.out.println(h);
+//        }
 
-    public static void main(String[] args) {
+        h = num % 100;
+        System.out.println(h);
+        num = num - h;
 
+        h = num % 100;
+        System.out.println(h);
+        num = num - h;
 
-
+        h = num % 100;
+        System.out.println(h);
+        num = num - h;
     }
 }
 
