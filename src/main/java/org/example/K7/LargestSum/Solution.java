f622d0968a2eb911204b2adc9b6c6af309f634a8
MarcinKrzciuk <martin.krzciuk@gmail.com>
2021-06-29 18:19:05 +0200

    test

---
+++ src/main/java/org/example/K7/LargestSum/Solution.java
@@ -0,0 +1,17 @@
+package org.example.K7.LargestSum;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public static int largestPairSum(int[] numbers){
+        List<Integer> intList = new ArrayList<Integer>(numbers.length);
+        for (int i : numbers) {
+            intList.add(i);
+        }
+        Collections.sort(intList);
+
+        return  intList.get(numbers.length -1) + intList.get(numbers.length -2);
+    }
+}
