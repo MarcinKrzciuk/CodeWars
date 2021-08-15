fe23a61871af564efa6281b9c24b7411485c97d3
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-15 07:21:51 +0200

    t

---
+++ src/main/java/org/example/K7/aerial_firefighting/Solution.java
@@ -0,0 +1,10 @@
+package org.example.K7.aerial_firefighting;
+
+public class Solution{
+    public static int waterbombs(String fire, int w){
+        //TODO: return the minimum required waterbombs to extinguish the fires in the array
+        return 0;
+    }
+}
+
+
