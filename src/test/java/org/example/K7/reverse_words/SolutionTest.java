0da2e1d19cdc6a8ababcaa6ea4ca380ad95ed55c
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-18 15:45:52 +0200

    update

---
+++ src/test/java/org/example/K7/reverse_words/SolutionTest.java
@@ -0,0 +1,17 @@
+package org.example.K7.reverse_words;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+    @Test
+    public void exampleCases() {
+        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", Kata.reverseWords("The quick brown fox jumps over the lazy dog."));
+        assertEquals("elppa", Kata.reverseWords("apple"));
+        assertEquals("a b c d", Kata.reverseWords("a b c d"));
+        assertEquals("elbuod  decaps  sdrow", Kata.reverseWords("double  spaced  words"));
+    }
+}
