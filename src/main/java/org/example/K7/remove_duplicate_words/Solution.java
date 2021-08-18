8da3fc9c68ace2a4812e3890099bf6fb9df16cf6
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-18 15:46:24 +0200

    t

--- src/main/java/org/example/K7/remove_duplicate_words/Solution.java
+++ src/main/java/org/example/K7/remove_duplicate_words/Solution.java
@@ -1,14 +1,10 @@
 package org.example.K7.remove_duplicate_words;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 class Solution{
     public static String removeDuplicateWords(String s){
-        String[] splited = s.split("\\s+");
-        List<String> checked = Arrays.asList(splited);
-        checked = checked.stream().distinct().collect(Collectors.toList());
-        return String.join(" ", checked);
+        return Arrays.stream(s.split(" ")).distinct().collect(Collectors.joining(" "));
     }
 }
