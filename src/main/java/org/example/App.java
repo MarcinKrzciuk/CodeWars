de06fc36ba4a4cb26500be9f9b01d0cc43e1d88c
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-18 15:46:33 +0200

    t

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,47 +1,20 @@
 package org.example;
 
-
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws ParseException {
-//        int [] array = new int[]{1,11,3,9};
-//        ArrayList<String> list = new ArrayList<>();
-//
-//        String[] onedigit = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-//        String[] twodigits = new String[] {"", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-//        String[] multipleoftens = new String[] {"",  "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-//        String[] poweroftens = new String[] {"Hundred", "Thousand"};
-//
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] < 10) {
-//                list.add(onedigit[array[i]]);
-//            }
-//        }
-//
-//        System.out.println(list);
 
-        int num = 999;
-        int h;
-        ArrayList<Integer> list = new ArrayList<>();
-        String nr = String.valueOf(num);
-//        for (int i = 0; i < nr.length(); i++) {
-//            h = num % 10;
-//            System.out.println(h);
-//        }
+        String original = "The quick brown fox jumps over the lazy dog.";
+        StringBuilder text = new StringBuilder(original);
 
-        h = num % 100;
-        System.out.println(h);
-        num = num - h;
 
-        h = num % 100;
-        System.out.println(h);
-        num = num - h;
+        List<String> finish = new ArrayList<>();
+        for  (int i = 0; i < original.length(); i++) {
+        }
 
-        h = num % 100;
-        System.out.println(h);
-        num = num - h;
     }
 }
 
