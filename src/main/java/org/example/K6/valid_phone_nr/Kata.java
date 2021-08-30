dad86ac3ca35bb4350a4281e836c509a60762441
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-30 16:01:17 +0200

    t

---
+++ src/main/java/org/example/K6/valid_phone_nr/Kata.java
@@ -0,0 +1,8 @@
+package org.example.K6.valid_phone_nr;
+
+public class Kata {
+    public static boolean validPhoneNumber(String phoneNumber) {
+        // TODO: Return whether phoneNumber is in the proper form
+        return phoneNumber.matches("\\(\\d{3}\\) \\d{3}-\\d{4}");
+    }
+}
