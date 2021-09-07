94e6faf2e3ba0cb34de6792f52679ad36946ade6
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-09-07 19:16:14 +0200

    t

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,24 +1,33 @@
 package org.example;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class App {
+
     public static void main(String[] args) throws ParseException {
 
-        String original = "The quick brown fox jumps over the lazy dog.";
-        StringBuilder text = new StringBuilder(original);
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        int runs = 1;
 
+        List<Integer> result = new ArrayList<>();
 
-        List<String> finish = new ArrayList<>();
-        for  (int i = 0; i < original.length(); i++) {
+        int middle = array.length / 2;
+        System.out.println(middle);
+
+        if (array.length % 2 != 0) {
+            for (int i = 0; i < middle; i++) {
+                if (i < middle) {
+                    result.add(array[i] + array[array.length - 1 - i]);
+                }
+            }
+            runs--;
+            result.add(array[middle]);
         }
+
+
+
+        System.out.println(result);
 
     }
 }
-
-
-
-
-
