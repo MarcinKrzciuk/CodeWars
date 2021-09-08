45f00b66cac61774f2b66f12debcdf401048c5e0
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-09-08 06:43:34 +0200

    t

--- src/main/java/org/example/K6/fold_an_array/Kata.java
+++ src/main/java/org/example/K6/fold_an_array/Kata.java
@@ -2,6 +2,27 @@ package org.example.K6.fold_an_array;
 
 public class Kata {
     public static int[] foldArray(int[] array, int runs) {
-        return new int[] { 0 };
+        if (runs == 0) {
+            return array;
+        }
+
+        int[] tmp;
+        if (array.length % 2 == 0) {
+            tmp = new int[array.length / 2];
+            for (int i = 0; i < array.length / 2; i++) {
+                tmp[i] = array[i];
+            }
+        } else {
+            tmp = new int[array.length / 2 + 1];
+            for (int i = 0; i < array.length / 2 + 1; i++) {
+                tmp[i] = array[i];
+            }
+        }
+
+        for (int i = 0; i < array.length / 2; i++) {
+            tmp[i] += array[array.length - i - 1];
+        }
+        return foldArray(tmp, runs - 1);
+
     }
 }
